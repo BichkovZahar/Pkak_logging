039551cf36d251fc6e55b5e8c813521a48caf205
Asus <Bichk_di78@student.itstep.org>
2023-05-24 14:36:21 +0300

    2

--- main.py
+++ main.py
@@ -2,15 +2,12 @@ import logging
 def write_file(fill_path , data):
     try:
         with open(fill_path , 'w') as file:
+            logger = logging.getLogger("File")
+            logger.setLevel(logging.INFO)
+            logger.addHandler(logging.StreamHandler())
             file.write(data)
-            logging.basicConfig(level=logging.INFO,
-                                filename="logss.log",
-                                filemode='w',
-                                format='%(asctime)s - %(levelname)s - %(message)s')
-        logging.info(f"Дані з файлу {fill_path} успішно додано")
-        return data
-    except Exception as e:
-        logging.error(f'Сталася помилка під час  з файлу {fill_path}')
-        return None
-
+            logger.info("Успішно записали файл")
+    except Exception as a:
+        logger.error(f"В вашему файлі '{fill_path}' трапилась помилка -> {a}")
+logging.basicConfig(level=logging.INFO)
 write_file("output.txt" , input("Введіть що треба вписати файл: "))
